858d48467ee371865298b67c3c1e8021743e6248
Léa BARBEAU <leabarbeau@yahoo.fr>
2017-12-06 09:59:52 +0100

    appli-trello-api

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ let config = {
 
     // setup email data with unicode symbols
       mailOptions: {
-            host: 'smtp.office365.com', 
+            host: '', 
             port: 587,
             secure: false, // true for 465, false for other ports
             auth: {
-                user: 'send.mail@nouvellemarque.com', // generated ethereal user
-                pass: 'nmarque-2016'  // generated ethereal password
+                user: '', // generated ethereal user
+                pass: ''  // generated ethereal password
             },
             tls:{
                 rejectUnauthorized:false
             },
-            from: 'send.mail@nouvellemarque.com', // sender address
-            to: 'lea06485695+6ndaf8cebk2wyyx7kdji@boards.trello.com',
+            from: '', // sender address
+            to: '',
             subject: 'Carte envoyée par mail', // Subject line
       },   
 
